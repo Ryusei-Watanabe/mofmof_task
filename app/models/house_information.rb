cda62a2221587617cf1bbb25a3d3e082457ac23e
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-03 23:44:55 +0900

    バリデーション

--- app/models/house_information.rb
+++ app/models/house_information.rb
@@ -1,4 +1,11 @@
 class HouseInformation < ApplicationRecord
   has_many :near_stations, dependent: :destroy
   accepts_nested_attributes_for :near_stations, allow_destroy: true
+  with_options presence: true do
+    validates :property_name
+    validates :house_rent
+    validates :adsress
+    validates :age_of_a_build
+    validates :remark_column
+  end
 end
--- app/models/near_station.rb
+++ app/models/near_station.rb
@@ -1,5 +1,3 @@
 class NearStation < ApplicationRecord
    belongs_to :house_information, optional: true
-   validates :station_name, length:{in:0..10}
-
 end
